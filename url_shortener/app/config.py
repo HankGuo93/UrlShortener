11e19f74963a2123a1d3a7b98f200c9a9b538aa0
HankGuo <52509416+HankGuo93@users.noreply.github.com>
2024-08-06 18:33:18 +0800

    chore: added Dockerfile

--- url_shortener/app/config.py
+++ url_shortener/app/config.py
@@ -1,2 +1,4 @@
+import os
+
 PORT = 5066
-DATABASE_URL = f"postgresql://admin:admin@127.0.0.1:5432/url_shortener"
+DATABASE_URL = os.getenv('DATABASE_URL', f"postgresql://admin:admin@127.0.0.1:5432/url_shortener")
